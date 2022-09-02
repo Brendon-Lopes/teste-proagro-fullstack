2abc54540b673a9f295ee6e81312dfdb3486c1d7
Brendon <brendon.lopes.21@gmail.com>
2022-09-02 20:52:59 -0300

    refactor: :recycle: adds separate file for connection with database (mongoDB)

---
+++ backend/src/database/connection.py
@@ -0,0 +1,6 @@
+from server.instance import server
+from flask_pymongo import PyMongo
+from os import getenv
+
+mongo = PyMongo(server.app,
+    f"mongodb://{getenv('DB_HOST')}:{getenv('DB_PORT')}/{getenv('DB_NAME')}")
