59c3bd8f8365d4729b13ce607a492344dbbae320
Brendon <brendon.lopes.21@gmail.com>
2022-09-04 20:17:05 -0300

    feat: :sparkles: adds method to compare dates (JavaScript dates converted to ISO string).
    Also updates constant attributes (response messages related, and validation related)

--- backend/src/utils/index.py
+++ backend/src/utils/index.py
@@ -1,18 +1,19 @@
 from bson.json_util import default as json_default, dumps
+from datetime import datetime
 
 
 class Utils:
     CREATE_EVENT_REQUIRED_DATA = [
-        "nome",
-        "email",
         "cpf",
-        "localizacao",
-        "tipoLavroura",
         "dataColheita",
+        "email",
         "evento",
+        "localizacao",
+        "nome",
+        "tipoLavoura",
     ]
 
-    REQUIRED_DATA_ERROR_MESSAGE = "Os campos nome, email, cpf, localizacao, tipoLavroura, dataColheita e evento são obrigatórios"
+    REQUIRED_DATA_ERROR_MESSAGE = "Os campos nome, email, cpf, localizacao, tipoLavoura, dataColheita e evento são obrigatórios"
 
     LOCALIZACAO_REQUIRED_DATA = ["LAT", "LONG"]
 
@@ -20,10 +21,21 @@ class Utils:
         "Não foi encontrada comunicação de perda associada a esse id"
     )
 
+    CONFLICT_MESSAGE = "Evento diverge de comunicação registrada na mesma data"
+
     mimetype = "application/json"
 
-    def serializeList(obj):
+    def serialize_list(obj):
         return dumps([doc for doc in obj], default=json_default)
 
-    def serializeDict(obj):
+    def serialize_dict(obj):
         return dumps(obj, default=json_default)
+
+    def transform_date(date_string):
+        get_date = date_string.split("T")
+        year, month, day = get_date[0].split("-")
+        converted_data = datetime(int(year), int(month), int(day))
+        return converted_data
+
+    def compare_strings(string1, string2):
+        return string1.lower() == string2.lower()
