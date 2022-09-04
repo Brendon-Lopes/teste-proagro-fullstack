3cc8e87f6b9e9dd8aceef840eb7dbaead12bb60a
Brendon <brendon.lopes.21@gmail.com>
2022-09-03 23:19:39 -0300

    feat: :sparkles: adds post to route /loss-events
    method serialize was moved to Utils class, for organization purposes

--- backend/src/controllers/loss_events.py
+++ backend/src/controllers/loss_events.py
@@ -1,22 +1,30 @@
-from json import dumps
-from flask import Response
-from bson import json_util
-from flask_restx import Resource
+from http.client import BAD_REQUEST, INTERNAL_SERVER_ERROR
+from json import dumps, loads
 
+# from flask import Response
+from werkzeug import Response
+from flask_restx import Resource, abort
+from services.loss_events_service import LossEventsService
 from server.instance import server
+from utils.index import Utils
 
-from database.connection import mongo
+mimetype_json = Utils.mimetype
 
-app, api = server.app, server.api
+api = server.api
 
 
 @api.route("/loss-events")
 class LossEvents(Resource):
     def get(self):
-        cursor = mongo.db.lossEvents.find()
-        response = LossEvents.serialize(cursor)
-        return Response(response, mimetype="application/json", status=200)
+        try:
+            response, status = LossEventsService.get_all()
+            return Response(response, status, mimetype=mimetype_json)
+        except:
+            abort(INTERNAL_SERVER_ERROR, "Erro interno do servidor")
 
-    def serialize(obj):
-        return [dumps(doc, default=json_util.default) for doc in obj]
-
+    def post(self):
+        try:
+            response, status = LossEventsService.create(api.payload)
+            return Response(response, status, mimetype=mimetype_json)
+        except:
+            abort(BAD_REQUEST, "Erro interno do servidor")
