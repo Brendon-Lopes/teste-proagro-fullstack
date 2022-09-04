eec392b0fee62b3ef60f1f7cf4306df5bf524de6
Brendon <brendon.lopes.21@gmail.com>
2022-09-03 23:58:45 -0300

    feat: :sparkles: adds delete and put to route /loss-events/:id

--- backend/src/controllers/loss_events_by_id.py
+++ backend/src/controllers/loss_events_by_id.py
@@ -1,19 +1,24 @@
-from json import dumps
 from flask import Response
-from bson import json_util
 from flask_restx import Resource
-from bson.objectid import ObjectId
-
 from server.instance import server
+from services.loss_events_by_id_service import LossEventsByIdService
+from utils.index import Utils
 
-from database.connection import mongo
+mimetype_json = Utils.mimetype
 
-app, api = server.app, server.api
+api = server.api
 
 
 @api.route("/loss-events/<string:id>")
 class LossEventsById(Resource):
     def get(self, id):
-        cursor = mongo.db.lossEvents.find_one({"_id": ObjectId(id)})
-        response = dumps(cursor, default=json_util.default)
-        return Response(response, mimetype="application/json", status=200)
+        response, status = LossEventsByIdService.getById(id)
+        return Response(response, status, mimetype=mimetype_json)
+
+    def delete(self, id):
+        status = LossEventsByIdService.delete_one(id)
+        return Response(status=status)
+
+    def put(self, id):
+        response, status = LossEventsByIdService.update(id, api.payload)
+        return Response(response, status, mimetype=mimetype_json)
