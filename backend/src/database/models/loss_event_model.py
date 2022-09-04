371f5102916e74b4a65fead538141a8f5a4e856e
Brendon <brendon.lopes.21@gmail.com>
2022-09-04 20:08:41 -0300

    docs: :memo: updates API models for better documentation

--- backend/src/database/models/loss_event_model.py
+++ backend/src/database/models/loss_event_model.py
@@ -3,23 +3,32 @@ from flask_restx import fields
 
 api = server.api
 
+event_types = [
+    "CHUVA EXCESSIVA",
+    "GEADA",
+    "GRANIZO",
+    "SECA",
+    "VENDAVAL",
+    "RAIO",
+]
+
 location_fields = api.model(
-    "Location",
+    "Localizacao",
     {
-        "LAT": fields.Float,
-        "LONG": fields.Float,
+        "LAT": fields.Float(min=-90, max=90, required=True),
+        "LONG": fields.Float(min=-180, max=180, required=True),
     },
 )
 
 loss_event_fields = api.model(
-    "Loss Communication",
+    "Comunicacao de perda",
     {
-        "nome": fields.String,
-        "cpf": fields.String,
-        "email": fields.String,
-        "tipoLavroura": fields.String,
-        "dataColheita": fields.String,
-        "evento": fields.String,
-        "localizacao": fields.Nested(location_fields),
+        "nome": fields.String(required=True),
+        "cpf": fields.String(required=True),
+        "email": fields.String(required=True),
+        "tipoLavoura": fields.String(required=True),
+        "dataColheita": fields.DateTime(dt_format="iso8601", required=True),
+        "evento": fields.String(enum=event_types, required=True),
+        "localizacao": fields.Nested(location_fields, required=True),
     },
 )
