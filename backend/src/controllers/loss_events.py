cca91d1d26de976fdc4324f3149f8df053aacf6d
Brendon <brendon.lopes.21@gmail.com>
2022-09-04 00:49:01 -0300

    docs: :memo: adds docs to route

--- backend/src/controllers/loss_events.py
+++ backend/src/controllers/loss_events.py
@@ -1,12 +1,10 @@
 from http.client import BAD_REQUEST, INTERNAL_SERVER_ERROR
-from json import dumps, loads
-
-# from flask import Response
 from werkzeug import Response
 from flask_restx import Resource, abort
 from services.loss_events_service import LossEventsService
 from server.instance import server
 from utils.index import Utils
+from database.models.loss_event_model import loss_event_fields
 
 mimetype_json = Utils.mimetype
 
@@ -22,6 +20,7 @@ class LossEvents(Resource):
         except:
             abort(INTERNAL_SERVER_ERROR, "Erro interno do servidor")
 
+    @api.expect(loss_event_fields, validate=True, code=201)
     def post(self):
         try:
             response, status = LossEventsService.create(api.payload)
--- backend/src/controllers/loss_events_by_id.py
+++ backend/src/controllers/loss_events_by_id.py
@@ -3,6 +3,7 @@ from flask_restx import Resource
 from server.instance import server
 from services.loss_events_by_id_service import LossEventsByIdService
 from utils.index import Utils
+from database.models.loss_event_model import loss_event_fields
 
 mimetype_json = Utils.mimetype
 
@@ -10,6 +11,7 @@ api = server.api
 
 
 @api.route("/loss-events/<string:id>")
+@api.param("id", "ID da comunicação de perda")
 class LossEventsById(Resource):
     def get(self, id):
         response, status = LossEventsByIdService.getById(id)
@@ -19,6 +21,7 @@ class LossEventsById(Resource):
         status = LossEventsByIdService.delete_one(id)
         return Response(status=status)
 
+    @api.expect(loss_event_fields, validate=True, code=201)
     def put(self, id):
         response, status = LossEventsByIdService.update(id, api.payload)
         return Response(response, status, mimetype=mimetype_json)
