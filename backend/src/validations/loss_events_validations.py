56ae7b791d5efb2b13c61051f49d60bcb0079823
Brendon <brendon.lopes.21@gmail.com>
2022-09-03 23:20:41 -0300

    feat: :sparkles: adds class LossEventsValidations to centralize validation methods
    centralizes methods related to validation for the route /loss-event

---
+++ backend/src/validations/loss_events_validations.py
@@ -0,0 +1,31 @@
+from http.client import BAD_REQUEST
+from numpy import array_equal
+from flask_restx import abort
+from utils.index import Utils
+
+
+class LossEventsValidation:
+    def validate_create_data(data):
+        attributes = list(data.keys())
+
+        if not array_equal(attributes, Utils.CREATE_EVENT_REQUIRED_DATA):
+            abort(BAD_REQUEST, Utils.REQUIRED_DATA_ERROR_MESSAGE)
+
+        for key in attributes:
+            if len(data[key]) == 0:
+                abort(BAD_REQUEST, f"campo {key} não pode ser vazio")
+
+        if type(data["localizacao"]) != dict:
+            abort(BAD_REQUEST, "localizacao deve ser um objeto contendo LAT e LONG")
+
+        localizacao = list(data["localizacao"].keys())
+        if not array_equal(localizacao, Utils.LOCALIZACAO_REQUIRED_DATA):
+            abort(BAD_REQUEST, "localizacao deve conter LAT e LONG")
+
+        LAT, LONG = data["localizacao"]["LAT"], data["localizacao"]["LONG"]
+
+        if LAT > 90 or LAT < -90:
+            abort(BAD_REQUEST, "LAT deve estar entre -90 e 90")
+
+        if LONG > 180 or LONG < -180:
+            abort(BAD_REQUEST, "LONG deve estar entre -180 e 180")
