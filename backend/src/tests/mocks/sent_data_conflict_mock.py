09f3487ee1fe2d69dc49e6018a63d835b4c9fd98
Brendon <brendon.lopes.21@gmail.com>
2022-09-05 12:48:58 -0300

    test: :clown_face: adds mock for conflict sent data

---
+++ backend/src/tests/mocks/sent_data_conflict_mock.py
@@ -0,0 +1,9 @@
+SENT_DATA_CONFLICT_MOCK = {
+    "nome": "John Doe",
+    "email": "john@email.com",
+    "cpf": "12345678901",
+    "localizacao": {"LAT": 50.5, "LONG": 100.5},
+    "tipoLavoura": "soja",
+    "dataColheita": "2014-10-10T00:00:00.000Z",
+    "evento": "GEADA",
+}
