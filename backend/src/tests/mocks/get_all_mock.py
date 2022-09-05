e456656c66dc01392e6add962144992113d89a1a
Brendon <brendon.lopes.21@gmail.com>
2022-09-05 12:34:16 -0300

    feat: :clown_face: adds data mocks for testing purposes

---
+++ backend/src/tests/mocks/get_all_mock.py
@@ -0,0 +1,22 @@
+GET_ALL_MOCK = [
+    {
+        "_id": {"$oid": "6315900f5e781c7e48a5526b"},
+        "nome": "John Doe",
+        "email": "john@email.com",
+        "cpf": "12345678901",
+        "localizacao": {"LAT": 50.5, "LONG": 100.5},
+        "tipoLavoura": "soja",
+        "dataColheita": "2014-10-10T00:00:00.000Z",
+        "evento": "chuva excessiva",
+    },
+    {
+        "_id": {"$oid": "6315900f5e781c7e48a5526c"},
+        "nome": "Alan Turing",
+        "email": "alan@email.com",
+        "cpf": "12345678902",
+        "localizacao": {"LAT": 50.5, "LONG": 80.5},
+        "tipoLavoura": "arroz",
+        "dataColheita": "2014-10-10T00:00:00.000Z",
+        "evento": "geada",
+    },
+]
---
+++ backend/src/tests/mocks/sent_data_mock.py
@@ -0,0 +1,9 @@
+SENT_DATA_MOCK = {
+    "nome": "Alan Turing",
+    "email": "alan@email.com",
+    "cpf": "12345678902",
+    "localizacao": {"LAT": 50.5, "LONG": 80.5},
+    "tipoLavoura": "arroz",
+    "dataColheita": "2014-10-10T00:00:00.000Z",
+    "evento": "chuva excessiva",
+}
