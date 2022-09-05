9af00bdd22e0ce94e824f8c82f2a27752c7dac02
Brendon <brendon.lopes.21@gmail.com>
2022-09-05 12:52:38 -0300

    refactor: :fire: removes connection file
    it was removed since connection is now being set up inside Server class

--- backend/src/database/connection.py
+++
@@ -1,9 +0,0 @@
-from server.instance import server
-from flask_pymongo import PyMongo
-from os import getenv
-
-mongo = PyMongo(
-    server.app, f"mongodb://{getenv('DB_HOST')}:{getenv('DB_PORT')}/{getenv('DB_NAME')}"
-)
-
-loss_events_collection = mongo.db.lossEvents
