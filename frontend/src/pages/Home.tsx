928324b22ee3df288d450f6f8790d59cb98d047a
Brendon <brendon.lopes.21@gmail.com>
2022-09-06 01:10:43 -0300

    feat: :sparkles: adds initial API fetch setup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,31 @@
+import axios from 'axios';
 import { Nav } from 'components';
+import { Key, useEffect, useState } from 'react';
 
 export function Home() {
+  const [events, setEvents] = useState<{ nome: Key; _id: { $oid: Key } }[]>([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { data } = await axios.get('http://localhost:5000/loss-events');
+      setEvents(data);
+    };
+    fetchEvents();
+  }, []);
+
   return (
     <div>
       <Nav />
+      <ul>
+        {events.map((event) => (
+          <div key={event._id.$oid}>
+            <li>
+              id: {event._id.$oid}, nome: {event.nome}
+            </li>
+            <br />
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
