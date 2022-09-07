b1a41f24eeb753bbbce5ec6599e3e40aad84d9dc
Brendon <brendon.lopes.21@gmail.com>
2022-09-07 17:51:26 -0300

    feat: :sparkles: adds options to edit and delete on the details page of each document
    also adds CSS to details card

--- frontend/src/components/EventDetailsCard/EventDetailsCard.tsx
+++ frontend/src/components/EventDetailsCard/EventDetailsCard.tsx
@@ -1,20 +1,151 @@
 import { IEvent } from 'interfaces/IEvent';
+import { useNavigate } from 'react-router-dom';
 
-export function EventDetailsCard(event: IEvent) {
+type Props = {
+  event: IEvent;
+  setDisplay: any;
+};
+
+export function EventDetailsCard({ event, setDisplay }: Props) {
+  const navigate = useNavigate();
+
+  const formatDate = (date: string) => {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleDateString('pt-BR');
+  };
+
   return (
-    <section>
-      <h1>Details</h1>
-      <p>id: {event._id.$oid}</p>
-      <p>Nome: {event.nome}</p>
-      <p>Email: {event.email}</p>
-      <p>CPF: {event.cpf}</p>
-      <p>Tipo da lavoura: {event.tipoLavoura}</p>
-      <p>Data colheita: {event.dataColheita}</p>
-      <p>evento ocorrido: {event.evento}</p>
-      <p>
-        Localização: Latitude: {event.localizacao.LAT}, Longitude:{' '}
-        {event.localizacao.LONG}
-      </p>
+    <section className="flex justify-center sm:mt-6">
+      <div className="overflow-hidden bg-white shadow sm:rounded-lg sm:w-3/5">
+        <div className="sm:flex sm:justify-between">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+              Detalhes comunicação de perda ID: {event._id.$oid}
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              Aqui se encontram todas as informações cadastradas para essa
+              comunicação.
+            </p>
+          </div>
+          <div>
+            <button
+              className="
+              h-10
+              inline-flex
+              justify-center
+              rounded-md
+              border
+              border-transparent
+              bg-green-600
+              py-2
+              px-4
+              text-sm
+              font-medium
+              text-white
+              shadow-sm
+              hover:bg-green-700
+              focus:outline-none
+              focus:ring-2
+              focus:ring-green-600
+              focus:ring-offset-2
+              mt-5
+              mr-6
+              "
+              onClick={() =>
+                navigate(`/editar/${event._id.$oid}`, { state: event })
+              }
+            >
+              Editar
+            </button>
+            <button
+              className="
+              h-10
+              inline-flex
+              justify-center
+              rounded-md
+              border
+              border-transparent
+              bg-red-600
+              py-2
+              px-4
+              text-sm
+              font-medium
+              text-white
+              shadow-sm
+              hover:bg-red-700
+              focus:outline-none
+              focus:ring-2
+              focus:ring-red-600
+              focus:ring-offset-2
+              mt-5
+              mr-5
+              "
+              onClick={() => setDisplay(true)}
+            >
+              Excluir
+            </button>
+          </div>
+        </div>
+        <div className="border-t border-gray-200">
+          <dl>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Nome</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {event.nome}
+              </dd>
+            </div>
+
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Email</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {event.email}
+              </dd>
+            </div>
+
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">CPF</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {event.cpf}
+              </dd>
+            </div>
+
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Tipo da lavoura
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {event.tipoLavoura}
+              </dd>
+            </div>
+
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Data da colheita
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {formatDate(event.dataColheita)}
+              </dd>
+            </div>
+
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Evento ocorrido
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {event.evento}
+              </dd>
+            </div>
+
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Localização</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                Latitude: {event.localizacao.LAT}, Longitude:{' '}
+                {event.localizacao.LONG}
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
     </section>
   );
 }
