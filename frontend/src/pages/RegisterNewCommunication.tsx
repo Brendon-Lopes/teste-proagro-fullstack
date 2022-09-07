fe688a61d3fb76ea9d99e1d688ad5130077a327e
Brendon <brendon.lopes.21@gmail.com>
2022-09-07 01:27:50 -0300

    feat: :sparkles: updates forms
    on RegisterNewCommunication.tsx page. And adds the function to post new documents to database

--- frontend/src/pages/RegisterNewCommunication.tsx
+++ frontend/src/pages/RegisterNewCommunication.tsx
@@ -1,11 +1,22 @@
 import { DateTimePicker, Nav } from 'components';
+import { IRegisterNewCommunication } from 'interfaces';
+import { useForm, FormProvider } from 'react-hook-form';
+import { EventsServices } from 'services/EventsServices';
+import { RegisterNewCommunicationResolver } from 'validations';
 
 export function RegisterNewCommunication() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const data = Object.fromEntries(formData);
-    console.log(data);
+  const formMethods = useForm<IRegisterNewCommunication>({
+    resolver: RegisterNewCommunicationResolver,
+  });
+
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = formMethods;
+
+  const onSubmit = (data: any) => {
+    EventsServices.create(data);
   };
 
   return (
@@ -13,7 +24,7 @@ export function RegisterNewCommunication() {
       <Nav />
 
       <div className="flex justify-center mt-10">
-        <div className="w-3/4">
+        <div className="sm:w-3/4">
           <div className="md:grid md:grid-cols-3 md:gap-6">
             <div className="md:col-span-1">
               <div className="px-4 sm:px-0">
@@ -28,128 +39,191 @@ export function RegisterNewCommunication() {
               </div>
             </div>
             <div className="mt-5 md:col-span-2 md:mt-0">
-              <form onSubmit={handleSubmit}>
-                <div className="overflow-hidden shadow sm:rounded-md">
-                  <div className="bg-white px-4 py-5 sm:p-6">
-                    <div className="grid grid-cols-6 gap-6">
-                      <div className="col-span-6 sm:col-span-5">
-                        <label
-                          htmlFor="nome"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Nome
-                        </label>
-                        <input
-                          type="text"
-                          name="nome"
-                          id="nome"
-                          autoComplete="given-name"
-                          className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
-                        />
-                      </div>
+              <FormProvider {...formMethods}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="overflow-hidden shadow sm:rounded-md">
+                    <div className="bg-white px-4 py-5 sm:p-6">
+                      <div className="grid grid-cols-6 gap-6">
+                        <div className="col-span-6 sm:col-span-5">
+                          <label
+                            htmlFor="nome"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Nome
+                          </label>
+                          <input
+                            {...register('nome')}
+                            type="text"
+                            name="nome"
+                            id="nome"
+                            autoComplete="given-name"
+                            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
+                          />
+                          {errors?.nome?.message && (
+                            <p className="error-message">
+                              {errors?.nome?.message}
+                            </p>
+                          )}
+                        </div>
+                        <div className="col-span-6 sm:col-span-5">
+                          <label
+                            htmlFor="email"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Email
+                          </label>
+                          <input
+                            {...register('email')}
+                            type="text"
+                            name="email"
+                            id="email"
+                            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
+                          />
+                          {errors?.email?.message && (
+                            <p className="error-message">
+                              {errors?.email?.message}
+                            </p>
+                          )}
+                        </div>
 
-                      <div className="col-span-6 sm:col-span-5">
-                        <label
-                          htmlFor="email"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Email
-                        </label>
-                        <input
-                          type="text"
-                          name="email"
-                          id="email"
-                          className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
-                        />
-                      </div>
+                        <div className="col-span-6 sm:col-span-5">
+                          <label
+                            htmlFor="cpf"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            CPF
+                          </label>
+                          <input
+                            {...register('cpf')}
+                            type="number"
+                            name="cpf"
+                            id="cpf"
+                            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
+                          />
+                          {errors?.cpf?.message && (
+                            <p className="error-message">
+                              {errors?.cpf?.message}
+                            </p>
+                          )}
+                        </div>
 
-                      <div className="col-span-6 sm:col-span-5">
-                        <label
-                          htmlFor="cpf"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          CPF
-                        </label>
-                        <input
-                          type="text"
-                          name="cpf"
-                          id="cpf"
-                          className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
-                        />
-                      </div>
+                        <div className="col-span-6 sm:col-span-5">
+                          <label
+                            htmlFor="lavoura"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Tipo de lavoura
+                          </label>
+                          <input
+                            {...register('lavoura')}
+                            type="text"
+                            name="lavoura"
+                            id="lavoura"
+                            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
+                          />
+                          {errors?.lavoura?.message && (
+                            <p className="error-message">
+                              {errors?.lavoura?.message}
+                            </p>
+                          )}
+                        </div>
 
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="latitude"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Latitude
-                        </label>
-                        <input
-                          type="number"
-                          name="latitude"
-                          id="latitude"
-                          className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
-                        />
-                      </div>
+                        <div className="col-span-6 sm:col-span-3">
+                          <label
+                            htmlFor="latitude"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Latitude
+                          </label>
+                          <input
+                            {...register('latitude')}
+                            type="number"
+                            name="latitude"
+                            id="latitude"
+                            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
+                          />
+                          {errors?.latitude?.message && (
+                            <p className="error-message">
+                              {errors?.latitude?.message}
+                            </p>
+                          )}
+                        </div>
 
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="longitude"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Longitude
-                        </label>
-                        <input
-                          type="number"
-                          name="longitude"
-                          id="longitude"
-                          className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
-                        />
-                      </div>
+                        <div className="col-span-6 sm:col-span-3">
+                          <label
+                            htmlFor="longitude"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Longitude
+                          </label>
+                          <input
+                            {...register('longitude')}
+                            type="number"
+                            name="longitude"
+                            id="longitude"
+                            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-sm p-2"
+                          />
+                          {errors?.longitude?.message && (
+                            <p className="error-message">
+                              {errors?.longitude?.message}
+                            </p>
+                          )}
+                        </div>
 
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="evento"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Evento ocorrido
-                        </label>
-                        <select
-                          id="evento"
-                          name="evento"
-                          className="mt-1 block w-full rounded-md border border-gray-300 bg-white p-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 text-sm"
-                        >
-                          <option>CHUVA EXCESSIVA</option>
-                          <option>GEADA</option>
-                          <option>GRANIZO</option>
-                          <option>SECA</option>
-                          <option>VENDAVAL</option>
-                          <option>RAIO</option>
-                        </select>
-                      </div>
+                        <div className="col-span-6 sm:col-span-3">
+                          <label
+                            htmlFor="evento"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Evento ocorrido
+                          </label>
+                          <select
+                            {...register('evento')}
+                            id="evento"
+                            name="evento"
+                            className="mt-1 block w-full rounded-md border border-gray-300 bg-white p-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 text-sm"
+                          >
+                            <option>CHUVA EXCESSIVA</option>
+                            <option>GEADA</option>
+                            <option>GRANIZO</option>
+                            <option>SECA</option>
+                            <option>VENDAVAL</option>
+                            <option>RAIO</option>
+                          </select>
+                          {errors?.evento?.message && (
+                            <p className="error-message">
+                              {errors?.evento?.message}
+                            </p>
+                          )}
+                        </div>
 
-                      <div className="col-span-6 sm:col-span-5">
-                        <label
-                          htmlFor="data"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Data
-                        </label>
-                        <DateTimePicker />
+                        <div className="col-span-6 sm:col-span-5">
+                          <label
+                            htmlFor="data"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Data
+                          </label>
+                          <DateTimePicker name="data" />
+                          {errors?.data?.message && (
+                            <p className="error-message">
+                              {errors?.data?.message}
+                            </p>
+                          )}
+                        </div>
                       </div>
                     </div>
+                    <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                      <button
+                        type="submit"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      >
+                        Save
+                      </button>
+                    </div>
                   </div>
-                  <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                      Save
-                    </button>
-                  </div>
-                </div>
-              </form>
+                </form>
+              </FormProvider>
             </div>
           </div>
         </div>
