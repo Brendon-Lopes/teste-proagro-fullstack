5da3998d275037fd869900889c420909da2a1173
Brendon <brendon.lopes.21@gmail.com>
2022-09-07 01:26:01 -0300

    feat: :sparkles: updates services to communicate with backend
    adds the "create" function to post new documents to the database

--- frontend/src/services/EventsServices.ts
+++ frontend/src/services/EventsServices.ts
@@ -1,9 +1,29 @@
+import { ISendEvent } from 'interfaces';
 import { Api } from 'providers';
 
 const getAll = () => Api.get('/loss-events');
+
 const getById = (id: string) => Api.get(`/loss-events/${id}`);
+
+const create = (data: ISendEvent) => {
+  const formattedData = {
+    nome: data.nome,
+    email: data.email,
+    cpf: data.cpf,
+    tipoLavoura: data.lavoura,
+    dataColheita: new Date(data.data).toISOString(),
+    evento: data.evento,
+    localizacao: {
+      LAT: +data.latitude,
+      LONG: +data.longitude,
+    },
+  };
+
+  Api.post('/loss-events', formattedData);
+};
 
 export const EventsServices = {
   getAll,
   getById,
+  create,
 };
